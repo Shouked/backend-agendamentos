e40d7ee938b56d127abc4b827cf0f1935cbf7975
Shouked <136965203+Shouked@users.noreply.github.com>
2025-02-24 13:57:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const sgMail = require('@sendgrid/mail'); // Adicionando SendGrid
+const sgMail = require('@sendgrid/mail');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-// Conexão com MongoDB Atlas
 mongoose.connect('mongodb+srv://iagofonseca:Toldo+10@cluster0.oo8my.mongodb.net/agendamentos?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,31 +14,27 @@ mongoose.connect('mongodb+srv://iagofonseca:Toldo+10@cluster0.oo8my.mongodb.net/
 .then(() => console.log('Conectado ao MongoDB!'))
 .catch(err => console.log('Erro ao conectar:', err));
 
-// Configurar SendGrid
-sgMail.setApiKey('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'); // Substitua pela chave do SendGrid
+sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Usando variável de ambiente
 
-// Schema do agendamento
 const agendamentoSchema = new mongoose.Schema({
   procedimento: String,
   data: String,
   horario: String,
   cliente: String,
   telefone: String,
-  dataCriacao: { type: Date, default: Date.now } // Adicionando dataCriacao
+  dataCriacao: { type: Date, default: Date.now }
 });
 
 const Agendamento = mongoose.model('Agendamento', agendamentoSchema);
 
-// Rota para criar agendamento
 app.post('/agendamentos', async (req, res) => {
-  const { procedimento, data, horario, cliente, telefone } = req.body; // Adicionando telefone
+  const { procedimento, data, horario, cliente, telefone } = req.body;
   const novoAgendamento = new Agendamento({ procedimento, data, horario, cliente, telefone });
   await novoAgendamento.save();
 
-  // Enviar e-mail
   const msg = {
-    to: 'iagofonseca1992@hotmail.com', // Seu e-mail pessoal
-    from: 'iagofonseca1992@hotmail.com', // E-mail verificado no SendGrid
+    to: 'iagofonseca1992@hotmail.com', // Substitua pelo seu e-mail
+    from: 'iagofonseca1992@hotmail.com', // Substitua pelo e-mail verificado
     subject: 'Novo Agendamento Criado',
     text: `Um novo agendamento foi feito!\n\nProcedimento: ${procedimento}\nData: ${data}\nHorário: ${horario}\nCliente: ${cliente}\nTelefone: ${telefone}\nCriado em: ${novoAgendamento.dataCriacao}`
   };
@@ -49,7 +44,6 @@ app.post('/agendamentos', async (req, res) => {
   res.status(201).send('Agendamento criado com sucesso!');
 });
 
-// Rota para listar agendamentos
 app.get('/agendamentos', async (req, res) => {
   const agendamentos = await Agendamento.find();
   res.json(agendamentos);
