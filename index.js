39453c17ffb3d91d6c157c5e7819c664fafc6cf3
Shouked <136965203+Shouked@users.noreply.github.com>
2025-02-25 09:30:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Conexão com MongoDB Atlas
 mongoose.connect('mongodb+srv://iagofonseca:Toldo+10@cluster0.oo8my.mongodb.net/agendamentos?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,10 +14,8 @@ mongoose.connect('mongodb+srv://iagofonseca:Toldo+10@cluster0.oo8my.mongodb.net/
 .then(() => console.log('Conectado ao MongoDB!'))
 .catch(err => console.log('Erro ao conectar:', err));
 
-// Configurar SendGrid
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Schema do agendamento
 const agendamentoSchema = new mongoose.Schema({
   procedimento: String,
   data: String,
@@ -28,16 +25,14 @@ const agendamentoSchema = new mongoose.Schema({
   dataCriacao: { type: Date, default: Date.now }
 });
 
-// Schema do usuário (proprietário)
 const usuarioSchema = new mongoose.Schema({
   email: String,
-  senha: String // Senha simples por agora, sem criptografia
+  senha: String
 });
 
 const Agendamento = mongoose.model('Agendamento', agendamentoSchema);
 const Usuario = mongoose.model('Usuario', usuarioSchema);
 
-// Rota de login
 app.post('/login', async (req, res) => {
   const { email, senha } = req.body;
   const usuario = await Usuario.findOne({ email, senha });
@@ -48,15 +43,14 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Rota para criar agendamento
 app.post('/agendamentos', async (req, res) => {
   const { procedimento, data, horario, cliente, telefone } = req.body;
   const novoAgendamento = new Agendamento({ procedimento, data, horario, cliente, telefone });
   await novoAgendamento.save();
 
   const msg = {
-    to: 'kingshowk23@gmail.com', // Substitua pelo seu e-mail pessoal
-    from: 'iagofonseca1992@hotmail.com', // Substitua pelo e-mail verificado no SendGrid
+    to: 'kingshowk23@gmail.com',
+    from: 'iagofonseca1992@hotmail.com',
     subject: 'Novo Agendamento Criado',
     text: `Um novo agendamento foi feito!\n\nProcedimento: ${procedimento}\nData: ${data}\nHorário: ${horario}\nCliente: ${cliente}\nTelefone: ${telefone}\nCriado em: ${novoAgendamento.dataCriacao}`
   };
@@ -67,7 +61,6 @@ app.post('/agendamentos', async (req, res) => {
   res.status(201).send('Agendamento criado com sucesso!');
 });
 
-// Rota para listar agendamentos (sem autenticação por agora)
 app.get('/agendamentos', async (req, res) => {
   const agendamentos = await Agendamento.find();
   res.json(agendamentos);
